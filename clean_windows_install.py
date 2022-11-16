2f1280131b1f71ee3eec85c78c7fd8c5051212f4
Kyle Stevens <43458322+kyle-stevens@users.noreply.github.com>
2022-11-16 16:50:14 -0700

    Adding in Recycle Bin and Old Windows Installs Features

---
+++ clean_windows_install.py
@@ -0,0 +1,24 @@
+import sys
+import os
+import platform
+import subprocess
+import shutil
+import winshell
+
+def clean_windows_install():
+    system_platform = platform.system()
+    if system_platform == 'Windows':
+        os.chdir('C:\\')
+        try:
+            shutil.rmtree('Windows.old')
+        except FileNotFoundError:
+            print('STATUS:\tNo Old Windows Installations to Delete.')
+    return
+
+def empty_recycle_bin():
+    winshell.recycle_bin().empty(confirm=False, show_progress=False, sound=False)
+    print('STATUS\tRecycle Bin has been emptied.')
+    return
+
+if __name__ == '__main__':
+    clean_system_temp_files()
